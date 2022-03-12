64f8f982822a95016e5a352ef25b2c763bf05f34
Maarten Mulders <m.th.mulders@xs4all.nl>
2022-03-12 22:24:38 +0100

    Prefer local copies of the fonts

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   plugins: [
     require('postcss-import'),
-    require('postcss-import-url')({
-      'modernBrowser': true
-    }),
     require('tailwindcss'),
     require('autoprefixer'),
   ]
